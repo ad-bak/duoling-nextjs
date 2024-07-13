9996388c151c4c4893be83b2ed1026662ec05787
Adnan <adbak.dev@gmail.com>
2024-07-13 22:29:06 +0200

    mobile

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,17 @@
-function Sidebar() {
-  return <div>Sidebar</div>;
+import { cn } from "@/lib/utils";
+
+type Props = {
+  className?: string;
+};
+
+function Sidebar({ className }: Props) {
+  return (
+    <div
+      className={cn("flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top- px-4 border-r-2 flex-col", className)}
+    >
+      Sidebar
+    </div>
+  );
 }
 
 export default Sidebar;
