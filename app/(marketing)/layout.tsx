249cc2acbbd7be643ceb388f6bbc72b2f805efd8
Adnan <adbak.dev@gmail.com>
2024-07-13 21:42:55 +0200

    land pg

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "./header";
+
+export default function layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
+}
