b4020ab2d066476fa50216cf1dee7f8e920635c6
Adnan <adbak.dev@gmail.com>
2024-07-14 23:41:30 +0200

    sidebar

--- components/sidebar-item.tsx
+++ components/sidebar-item.tsx
@@ -2,6 +2,8 @@
 
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 type Props = {
   label: string;
@@ -11,10 +13,20 @@ type Props = {
 
 function SidebarItem({ label, iconSrc, href }: Props) {
   const pathname = usePathname();
+  const active = pathname === href;
   return (
-    <Button variant="outline" className="uppercase shadow-md">
-      {label}
-    </Button>
+    <>
+      <Button
+        variant="outline"
+        className={`flex items-center gap-x-2 w-full justify-start h-[52px] ${active ? "bg-green-500 text-white" : ""}`}
+        asChild
+      >
+        <Link href={href}>
+          <Image src={iconSrc} height={32} width={32} alt={label} />
+          {label}
+        </Link>
+      </Button>
+    </>
   );
 }
 
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,8 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import SidebarItem from "./sidebar-item";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
   className?: string;
@@ -17,7 +19,18 @@ function Sidebar({ className }: Props) {
         </div>
       </Link>
       <div className="flex flex-col gap-y-2 flex-1">
-        <SidebarItem label="learn" />
+        <SidebarItem label="learn" href="/learn" iconSrc="/learn.svg" />
+        <SidebarItem label="leaderbord" href="/leaderboard" iconSrc="/leaderboard.svg" />
+        <SidebarItem label="quests" href="/quests" iconSrc="/quests.svg" />
+        <SidebarItem label="shop" href="/shop" iconSrc="/shop.svg" />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton />
+        </ClerkLoaded>
       </div>
     </div>
   );
