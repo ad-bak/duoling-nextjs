0a614c979ab5ea8394d695e619fbfc7eff30d1ac
Adnan <adbak.dev@gmail.com>
2024-07-18 20:51:59 +0200

    wrapper

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,22 @@
+import FeedWrapper from "@/components/feed-wrapper";
 import SitckyWrapper from "@/components/sticky-wrapper";
+import Header from "./header";
+import UserProgress from "@/components/user-progress";
 
 function LearnPage() {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
-      <SitckyWrapper />
+      <SitckyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={true}
+        ></UserProgress>
+      </SitckyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
     </div>
   );
 }
--- components/sticky-wrapper.tsx
+++ components/sticky-wrapper.tsx
@@ -1,5 +1,13 @@
-function SitckyWrapper() {
-  return <div>SitckyWrapper</div>;
+type Props = {
+  children: React.ReactNode;
+};
+
+function SitckyWrapper({ children }: Props) {
+  return (
+    <div className="hidden lg:block w-[368px] sticky self-end bottom-6">
+      <div className="min-h-[calc(100vh-48px)] sticky top-6 flex flex-col gap-y-4">{children}</div>
+    </div>
+  );
 }
 
 export default SitckyWrapper;
