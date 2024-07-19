ccf2c14d2dfe1b69b5e29b376f79af251163f2a6
Adnan <adbak.dev@gmail.com>
2024-07-19 22:12:01 +0200

    check

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -7,7 +7,7 @@ async function CoursePage() {
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-stone-700">Language Course</h1>
 
-      <List courses={courses} activeCourse={1} />
+      <List courses={courses} activeCourseId={1} />
     </div>
   );
 }
