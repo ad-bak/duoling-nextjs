8d91a56520258dc84a368fd251a285e4484fe265
Adnan <adbak.dev@gmail.com>
2024-07-22 23:02:22 +0200

    test empty commit

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -4,7 +4,6 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "../db/schema";
 
 const client = postgres(process.env.DATABASE_URL!);
-
 const db = drizzle(client, { schema });
 
 const main = async () => {
