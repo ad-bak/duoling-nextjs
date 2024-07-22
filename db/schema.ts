11ec96c78547f15d24f4e63929d0b77825ebf445
Adnan <adbak.dev@gmail.com>
2024-07-23 01:04:45 +0200

    rename

--- db/schema.ts
+++ db/schema.ts
@@ -68,7 +68,7 @@ export const challengesRelations = relations(challenges, ({ one, many }) => ({
   challengeProgress: many(challengeProgress),
 }));
 
-export const challengeOptions = pgTable("challengeOptions", {
+export const challengeOptions = pgTable("challenge_options", {
   id: serial("id").primaryKey(),
   challengeId: integer("challenge_id")
     .references(() => challenges.id, { onDelete: "cascade" })
@@ -86,7 +86,7 @@ export const challengeOptionsRelations = relations(challengeOptions, ({ one, man
   }),
 }));
 
-export const challengeProgress = pgTable("challengeProgress", {
+export const challengeProgress = pgTable("challenge_progress", {
   id: serial("id").primaryKey(),
   userId: text("user_id").notNull(),
   challengeId: integer("challenge_id")
