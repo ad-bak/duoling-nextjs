35ac2fb8bebde7f331b589e9cc07119b05f5d3ce
Adnan <adbak.dev@gmail.com>
2024-07-23 23:21:51 +0200

    seed

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -12,6 +12,10 @@ const main = async () => {
 
     await db.delete(schema.userProgress);
     await db.delete(schema.courses);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeProgress);
     console.log("Deleted all records");
 
     await db.insert(schema.courses).values([
@@ -34,6 +38,62 @@ const main = async () => {
         id: 4,
         title: "Croatian",
         imageSrc: "/hr.svg",
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: "Unit 1",
+        description: "Introduction to Spanish",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        title: "Nouns",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: "SELECT",
+        order: 1,
+        question: `What is the Spanish word for "man"?`,
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: "/man.svg",
+        correct: true,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: "/woman.svg",
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: "/robot.svg",
+        correct: false,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
       },
     ]);
 
